1c8907cefb789d1e6a82505777249281fa687012
SeekurJr <seekurjr@SeekurJr>
2013-01-25 14:25:16 +0100

    modified test script and added some test for sin sweep

--- scripts/test_ptu.rb
+++ scripts/test_ptu.rb
@@ -26,27 +26,69 @@ Orocos.run 'ptu_directedperception::DirectedPerceptionTask' => 'PTUTask' do
     ptu.configure
     ptu.start
 
-    for i in 0..22
+    (1..10).each do |i|
         if rot = rot_reader.read()
             ea = ptu.ptFromRBS(rot)
-            puts "#{i} pan: #{ea.data[0]} tilt: #{ea.data[1]}"
+            puts "pan: #{ea.data[0]} tilt: #{ea.data[1]}"
         end
         sleep(0.1)
     end
+    
 
     Readline.readline("Press <Enter> to test movement.") do
     end
 
     puts "Going to 45 deg, 22.5 deg"
-    rot = ptu.rbsFromPanTilt(0.7071,0.35355)
+    rot = ptu.rbsFromPanTilt(0.7071,-0.35355)
     rot_writer.write(rot)
     
-    sleep(10.0)
+    t0 = Time.now
+    while ( Time.now - t0 < 2.0 )
+        if rot = rot_reader.read()
+            ea = ptu.ptFromRBS(rot)
+            puts "pan: #{ea.data[0]} tilt: #{ea.data[1]}"
+        end
+        sleep(0.01)
+    end
 
     puts "Going back"
     rot = ptu.rbsFromPanTilt(0,0)
     rot_writer.write(rot)
 
-    sleep(5.0)
+    sleep(2.0)
+
+    start_swp = 0.35355 # 22.5 deg
+    end_swp = -0.35355 # -22.5 deg
+
+    Readline.readline("Press <Enter> to test sweeping.") do
+    end
+
+    def tilt(rot,ptu)
+        if rot
+            ptu.ptFromRBS(rot).data[1]
+        else
+            nil
+        end
+    end
+
+    pos = start_swp
+    next_pos = end_swp
+    while true
+        rot = ptu.rbsFromPanTilt(0,pos)
+        rot_writer.write(rot)
+        
+
+        tlt = tilt(rot_reader.read(),ptu)
+        while ( pos >= 0 && tlt < pos ) || ( pos < 0 && tlt > pos)
+            puts "tilt: #{tlt}"
+            sleep(0.01)
+            tlt = tilt(rot_reader.read(),ptu)
+        end
+
+        btwpos = pos
+        pos = next_pos
+        next_pos = btwpos
+    end
+
     puts "Fini"
 end
